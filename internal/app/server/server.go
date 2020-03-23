ddb4e3937df480ea9745cbbd81466f8ae1a01f3a
moguchev <abracadabra.14@mail.ru>
2020-03-23 15:23:30 +0300

    Database configuration

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,16 +1,17 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/log/logrusadapter"
 	"github.com/jackc/pgx/stdlib"
 	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/moguchev/UniBox/internal/app/users"
 	_usersHttpDeliver "github.com/moguchev/UniBox/internal/app/users/delivery/http"
 	_usersRepo "github.com/moguchev/UniBox/internal/app/users/repository"
 	_usersUcase "github.com/moguchev/UniBox/internal/app/users/usecase"
@@ -39,7 +40,7 @@ type MainServerOptions struct {
 // MainDBOptions -
 type MainDBOptions struct {
 	Host       string        `mapstructure:"host"`
-	Port       string        `mapstructure:"port"`
+	Port       uint16        `mapstructure:"port"`
 	User       string        `mapstructure:"user"`
 	Pass       string        `mapstructure:"pass"`
 	Name       string        `mapstructure:"name"`
@@ -47,14 +48,24 @@ type MainDBOptions struct {
 	AcqTimeout time.Duration `mapstructure:"acquire_timeout"`
 }
 
+type usecases struct {
+	users users.Usecase
+}
+
 // ConnectToDB - возвращает коннект к БД
 func ConnectToDB(opt MainDBOptions) (*sqlx.DB, error) {
-	uri := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-		opt.User, opt.Pass, opt.Host, opt.Port, opt.Name)
-	log.Debugf("DB URI: %s", uri)
-	config, err := pgx.ParseURI(uri)
-	if err != nil {
-		return nil, err
+	config := pgx.ConnConfig{
+		Host:     opt.Host,
+		Port:     opt.Port,
+		Database: opt.Name,
+		User:     opt.User,
+		Password: opt.Pass,
+		Logger:   logrusadapter.NewLogger(&log.Logger{}),
+		LogLevel: pgx.LogLevelInfo,
+		RuntimeParams: map[string]string{
+			"standard_conforming_strings": "on",
+		},
+		PreferSimpleProtocol: true, // протокол без prepare запроса
 	}
 
 	connPool, err := pgx.NewConnPool(pgx.ConnPoolConfig{
@@ -64,15 +75,16 @@ func ConnectToDB(opt MainDBOptions) (*sqlx.DB, error) {
 	})
 
 	if err != nil {
-		log.Fatalf("Failed to create connections pool: %s", err)
+		connPool.Close()
+		return nil, err
 	}
 
 	nativeDB := stdlib.OpenDBFromPool(connPool)
 	return sqlx.NewDb(nativeDB, "pgx"), nil
 }
 
-// NewRouter - returns router
-func NewRouter(opt Options) (*mux.Router, error) {
+// NewRouter - returns gorilla mux router
+func NewRouter(ucs usecases) *mux.Router {
 	router := mux.NewRouter()
 	router = router.PathPrefix("/api/").Subrouter()
 
@@ -82,28 +94,30 @@ func NewRouter(opt Options) (*mux.Router, error) {
 	router.Use(mw.CORSMiddleware)
 	router.Use(mw.RecoverMiddleware)
 
-	conn, err := ConnectToDB(opt.Database)
-	if err != nil {
-		return nil, err
-	}
+	_usersHttpDeliver.NewUsersHandler(router, ucs.users)
 
-	if err = conn.Ping(); err != nil {
-		return nil, err
-	}
-
-	uRepo := _usersRepo.NewUsersRepository(conn)
-	uCase := _usersUcase.NewUsersUsecase(uRepo, opt.Context.Timeout*time.Second)
-	_usersHttpDeliver.NewUsersHandler(router, uCase)
-
-	return router, nil
+	return router
 }
 
 // RunServer - запуск сервера
 func RunServer(opt Options) {
-	router, err := NewRouter(opt)
+	conn, err := ConnectToDB(opt.Database)
 	if err != nil {
-		log.Fatalf("Failed to create router: %s", err)
+		log.Fatal(err)
 	}
+
+	if err = conn.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	uRepo := _usersRepo.NewUsersRepository(conn)
+	uCase := _usersUcase.NewUsersUsecase(uRepo, opt.Context.Timeout*time.Second)
+
+	ucs := usecases{
+		users: uCase,
+	}
+
+	router := NewRouter(ucs)
 
 	srv := &http.Server{
 		Addr:         opt.Server.Address,
@@ -111,6 +125,7 @@ func RunServer(opt Options) {
 		ReadTimeout:  opt.Server.ReadTimeout * time.Second,
 		WriteTimeout: opt.Server.WriteTimeout * time.Second,
 	}
+
 	log.Infof("Server started at %s", opt.Server.Address)
 	log.Fatal(srv.ListenAndServe())
 }
