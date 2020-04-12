b67f8295f29d9571e5799b603bf6f3e33a5d4312
moguchev <abracadabra.14@mail.ru>
2020-04-12 16:49:08 +0300

    Copyright

--- internal/app/users/delivery/http/handler.go
+++ internal/app/users/delivery/http/handler.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020. Leonid Moguchev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package http
 
 import (
@@ -72,7 +88,7 @@ func (h *UsersHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}).Debug(newUser)
 
 	user, err := h.Usecase.CreateUser(ctx, newUser)
-	var code int
+	code := http.StatusCreated
 	if err != nil {
 		e := err.(models.Error)
 		switch e.Type {
@@ -90,7 +106,5 @@ func (h *UsersHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bytes, _ = json.Marshal(user)
-	w.WriteHeader(http.StatusCreated)
-	w.Write(bytes)
+	respond.Respond(w, r, code, user)
 }
